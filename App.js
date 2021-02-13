7cef2896af99dd23f55bb81d84776276aed4aa11
BbinMaharjan <bbinmaharjan@gmail.com>
2021-02-13 19:50:56 +0545

    FoodScren

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wellcome To My UI ApP</Text>
+      <Text>AmericanFood</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "skyblue",
+    backgroundColor: "yellow",
     alignItems: "center",
     justifyContent: "center",
   },
