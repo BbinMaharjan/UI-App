9be80d7db8991e86f755e8b6e8cb16b9aac22a1a
BbinMaharjan <bbinmaharjan@gmail.com>
2021-02-16 16:12:30 +0545

    FlatList  ScrollView

--- App.js
+++ App.js
@@ -1,21 +1,108 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  ImageBackground,
+  View,
+} from "react-native";
+import { FOODS } from "./data/data";
+
+const DATA = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Wellcome To My UI ApP</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <ScrollView
+        horizontal
+        style={styles.container}
+        showsHorizontalScrollIndicator={false}
+      >
+        {FOODS.map((food) => {
+          return (
+            <ImageBackground
+              key={food.title}
+              style={[styles.box]}
+              source={{ uri: food.image }}
+            >
+              <Text style={styles.title}>{food.title}</Text>
+            </ImageBackground>
+          );
+        })}
+      </ScrollView>
+
+      <SectionList
+        sections={DATA}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+        keyExtractor={(item, index) => item + index}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.heading}> {section.title} </Text>
+        )}
+      />
+
+      <FlatList
+        data={FOODS}
+        horizontal
+        renderItem={({ item }) => (
+          <ImageBackground
+            key={item.title}
+            style={[styles.box]}
+            source={{ uri: item.image }}
+          >
+            <Text style={styles.title}>{item.title}</Text>
+          </ImageBackground>
+        )}
+        keyExtractor={(item, index) => item.title + index}
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "skyblue",
+    backgroundColor: "#fff",
+    padding: 30,
+  },
+  box: {
+    height: 150,
+    width: 150,
+    marginRight: 10,
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: 10,
+  },
+  image: {
+    height: "100%",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  item: {
+    fontSize: 14,
+    marginLeft: 30,
   },
 });
---
+++ data/data.js
@@ -0,0 +1,58 @@
+const FOODS = [
+  {
+    title: "Burger",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+  {
+    title: "Sandwich",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+  {
+    title: "Momo",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+  {
+    title: "Chowmine",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+  {
+    title: "Sasague",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+  {
+    title: "Sekwaa",
+    price: "$20",
+    description: "this is a burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2Vyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    time: "20m",
+    rating: "4",
+  },
+];
+
+export { FOODS };
